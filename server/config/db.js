12e89b1af26926ce4e668b3d58a44b0d429715b9
Fortune Ogbooh <fortuneogbooh@gmail.com>
2026-07-14 18:28:49 +0100

    Set Postgres search path to public

--- server/config/db.js
+++ server/config/db.js
@@ -5,6 +5,7 @@ const connectionConfig = process.env.DATABASE_URL
   ? {
       connectionString: process.env.DATABASE_URL,
       ssl: process.env.DB_SSL === 'false' ? false : { rejectUnauthorized: false },
+      options: '-c search_path=public',
     }
   : {
       user: process.env.DB_USER,
@@ -13,6 +14,7 @@ const connectionConfig = process.env.DATABASE_URL
       password: String(process.env.DB_PASSWORD),
       port: process.env.DB_PORT,
       ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : undefined,
+      options: '-c search_path=public',
     };
 
 const pool = new Pool({
