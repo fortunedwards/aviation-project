a7bf1e43f22b69a712f04dbd5076c55773411464
Fortune Ogbooh <fortuneogbooh@gmail.com>
2026-07-31 14:29:48 +0100

    Use local hero image asset

--- client/src/data/images.js
+++ client/src/data/images.js
@@ -11,7 +11,7 @@ export const HOME_UPCOMING_TRAINING_IMAGES = [
   'https://lh3.googleusercontent.com/aida-public/AB6AXuB06V2eQJdYATqvCRvb0eyDw1FPMDsgY7qAd0H3EZ51buDEr2pnaJyN3l-rh_Wbnl9znfN6wsnq0Hx2EuSrXSPB-3f3dC7OC7EwGoPrd5CVdozy4Qx1d9RqRs0EzHTghr_0RfmaLw8JChCIzYYBEl3McbFdYiT93jj1qOam7CKrfxn3Xyo822qsms3S-X_KL1DwWKtLolzfY_HxajeT0e0GxmPrq4eiJ25CPRkB7c9qOpcr7eZQjdaOWuf4TWdfZVTixky2giDcbEn5',
 ];
 
-export const HOME_FEATURE_IMAGE = 'https://images.unsplash.com/photo-1504196606672-aef5c9cefc92?auto=format&fit=crop&w=1200&q=80';
+export const HOME_FEATURE_IMAGE = 'client/src/assets/images/home1.png';
 
 export const ABOUT_HERO_SLIDES = [
   'https://images.unsplash.com/photo-1544476915-ed1370594142?auto=format&fit=crop&w=1600&q=80',
